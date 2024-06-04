023c6a78e3e10727ad3c7996e3438dae440aa409
Clayton Strydom <claytonstrydom.10@gmail.com>
2024-06-04 16:01:38 +0200

    added navbar with functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,10 +11,13 @@ import FlexBetween from './FlexBetween'
 import { useDispatch } from 'react-redux';
 import { setMode } from 'state';
 import profileImage from "assets/tanjiro.png";
-import { AppBar, Toolbar, useTheme } from '@mui/material';
+import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
 
 
-const Navbar = () => {
+const Navbar = ({
+  isSidebarOpen,
+  setIsSidebarOpen,
+}) => {
     const dispatch = useDispatch();
     const theme = useTheme();
     
@@ -28,6 +31,36 @@ const Navbar = () => {
   >
     <Toolbar sx={{ justifyContent: "space-between" }}>
         { /* Left-side */}
+        <FlexBetween>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor ={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder='Search...' />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === 'dark' ? (
+              <DarkModeOutlined sx={{ fontSize: "25px"}} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px"}} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px"}} />
+          </IconButton>
+        </FlexBetween>
     </Toolbar>
   </AppBar>
   );
