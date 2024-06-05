4237e57fd85d5372a7ec4f4ba773c52c80c8a5b4
Clayton Strydom <claytonstrydom.10@gmail.com>
2024-06-05 09:14:26 +0200

    fixed bug with routes and added profile indicator for user

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -11,18 +11,20 @@ const Layout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const userId = useSelector((state) => state.global.userId);
   const { data } = useGetUserQuery(userId);
-  console.log(data);
+ 
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar 
+        user={data || {}}
         isNonMobile = {isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />
       <Box>
-        <Navbar 
+        <Navbar
+          user={data || {}} 
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
